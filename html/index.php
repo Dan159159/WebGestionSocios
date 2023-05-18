7d71f73cf85e1e8249493e3e2d22da10f38d74a9
Dan159159 <113427338+Dan159159@users.noreply.github.com>
2023-05-18 13:51:02 +0200

    directorys

---
+++ html/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poliderportivos Donnostia JAZ</title>
+</head>
+<body>
+    <header>
+        <nav>
+        <a class="nav-link" href="#" aria-current="page">Inicio</a>
+        <a class="nav-link" href="#">About me</a>
+        </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
