72e4b5ef726790da1e874847e09b006829ba08e4
DAN_casa <jazurduymontalvo@gmail.com>
2023-05-19 23:36:55 +0200

    1inttento

---
+++ html/crud/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../static/styleForm.css">
+    <title>Bienvenido ADMIN</title>
+</head>
+<body>
+<form class="login-form">
+    <div class="wrapper">
+        <h2>Iniciar sesión</h2>
+        <div class="form-group">
+            <label for="username">Usuario:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <button type="submit">Iniciar sesión</button>
+  </div>
+</form>
+
+</body>
+</html>
--- html/index.php
+++ html/index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./static/style.css">
     <title>Poliderportivos Donnostia JAZ</title>
 </head>
 <body>
+
     <header>
         <nav>
         <a class="nav-link" href="#" aria-current="page">Inicio</a>
