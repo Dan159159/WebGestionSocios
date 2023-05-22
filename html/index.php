a34c6fea6721d9ab1789af9402478b58241b5a4e
DAN_casa <jazurduymontalvo@gmail.com>
2023-05-22 22:43:48 +0200

    scripts confirmacion 22:43 22052023

--- html/index.php
+++ html/index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 function getPolideportivos() {
     $manager = new MongoDB\Driver\Manager("mongodb://root:password@mongo:27017");
 
@@ -38,118 +38,41 @@ function getPolideportivos() {
 
 // Obtener los datos de los polideportivos
 $polideportivos = getPolideportivos();
+*/
 
 
-//require_once('template.class.php');
 
-//new App();
+
 $uri = $_SERVER['REQUEST_URI'];
 $uriParts = explode('/', $uri);
 array_shift($uriParts);
 
-
-
+$titulo = 'Título de la página';
 switch ($uri) {
     case '/':
-  
+        ob_start();
+        include("./views/index.html");
+        include("./views/app.html");
+        $child = ob_get_clean();
         break;
-    
-    case '/crud':
-  
+    case '/about':
+        ob_start();
+        include("./views/about.html");
+        include("./views/app.html");
+        $child = ob_get_clean();
         break;
-    
+    default:
+        //Code
+        break;
 }
+/*$titulo = 'Título de la página';
+include("./views/index.html");
+include("./views/app.html");*/
+
+
+
+
 
 ?>
-<!doctype html>
-<html lang="en">
 
-<head>
-  <title><?= $titulo ?></title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-</head>
-
-<body>
-  <header>
-    <!-- place navbar here -->
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="javascript:void(0)">POLIDEPORTIVOS</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="mynavbar">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="javascript:void(0)">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="javascript:void(0)">Centres sportifs</a>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input class="form-control me-2" type="text" placeholder="Search">
-            <button class="btn btn-primary" type="button">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-  </header>
-  
-  <main>
-  <div class="container mt-3">
-    <div class="row">
-      <?php foreach ($polideportivos as $polideportivo) { ?>
-      <div class="col-md-4">
-        <div class="card mb-3">
-          <img class="card-img-top" src="./img/<?php echo $polideportivo['img']; ?>" alt="Card image">
-          <div class="card-body">
-            <h4 class="card-title"><?php echo $polideportivo['nombre']; ?></h4>
-            <p class="card-text"><strong>Localización:</strong> <?php echo $polideportivo['localizacion']; ?></p>
-            <a href="#" class="btn btn-primary"><strong>Teléfono:</strong> <?php echo $polideportivo['tlf']; ?></a>
-          </div>
-        </div>
-      </div>
-      <?php } ?>
-    </div>
-  </div>
-</main>
-
-  <footer>
-    <!-- place footer here -->
-    <div class="container">
-      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center">
-          <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-            <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-          </a>
-          <span class="text-muted">© 2021 Company, Inc</span>
-        </div>
-    
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-          <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-          <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-        </ul>
-      </footer>
-    </div>
-  </footer>
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
-</body>
-
-</html>   
 
